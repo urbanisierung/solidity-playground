3a65e37da2ac6e717e4d9c55efd722def369171e
Adam <i@adamurban.de>
2021-04-04 11:41:07 +0200

    add next iteration

--- inbox/compile.js
+++ inbox/compile.js
@@ -5,6 +5,38 @@ const solc = require('solc')
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
 const source = fs.readFileSync(inboxPath, 'utf8')
 
-console.log(solc.compile(source, 1))
+const input = {
+  language: 'Solidity',
+  sources: {
+    'Inbox.sol': {
+      content: source,
+    },
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+    },
+    outputSelection: {
+      '*': {
+        '*': ['*'],
+      },
+    },
+  },
+}
 
-module.exports = solc.compile(source, 1).contracts[':Inbox']
+//   solc.
+
+// const inbox = JSON.parse(solc.compile(JSON.stringify(input))).contracts.Inbox
+const compiled = JSON.parse(solc.compile(JSON.stringify(input)))
+module.exports = compiled.contracts['Inbox.sol'].Inbox
+
+// console.log(JSON.parse(solc.compile(JSON.stringify(input))).contracts.Inbox)
+
+// module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts.Inbox
+
+// console.log(JSON.parse(solc.compile(JSON.stringify(input))))
+// console.log(JSON.parse(solc.compile(input)))
+
+// console.log(solc.compile(source, 1))
+
+// module.exports = solc.compile(source, 1).contracts[':Inbox']
