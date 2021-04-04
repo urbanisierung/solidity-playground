c2cd90429ae69fb42afbb9e48832d1ba525a97ea
Adam <i@adamurban.de>
2021-04-04 11:25:11 +0200

    feat(inbox): first code

---
+++ inbox/contracts/Inbox.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.3;
+
+contract Inbox {
+    string public message;
+
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+    }
+    
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
